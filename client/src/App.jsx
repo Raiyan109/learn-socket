e19bbe6f81f1606030793eeb73b1d4ada594ad44
Raiyan109 <kraiyan109@gmail.com>
2024-12-29 12:51:56 +0600

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { io } from 'socket.io-client'
+
+function App() {
+  const socket = io('http://localhost:3000')
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default App

b24d4aa28076353cf2ad06f856c5b20c42564788
Raiyan109 <kraiyan109@gmail.com>
2025-01-02 10:17:28 +0600

    new chat ui

--- client/src/components/PrivateChat.jsx
+++ client/src/components/PrivateChat.jsx
@@ -28,7 +28,83 @@ const PrivateChat = ({ socket, username, room }) => {
 
     return (
         <div>
-            <div className="bg-white border border-slate-200 grid grid-cols-6 gap-2 rounded-xl p-2 text-sm">
+            <div className="flex flex-col items-center justify-center w-screen min-h-screen bg-gray-100 text-gray-800 p-10">
+
+                {/* <!-- Component Start --> */}
+                <div className="flex flex-col flex-grow w-full max-w-xl bg-white shadow-xl rounded-lg overflow-hidden">
+                    <div className="flex flex-col flex-grow h-0 p-4 overflow-auto">
+                        <div className="flex w-full mt-2 space-x-3 max-w-xs">
+                            <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+                            <div>
+                                <div className="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+                                    <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                </div>
+                                <span className="text-xs text-gray-500 leading-none">2 min ago</span>
+                            </div>
+                        </div>
+
+                        <div className="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
+                            <div>
+                                <div className="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
+                                    <p className="text-sm">Lorem ipsum dolor sit amet.</p>
+                                </div>
+                                <span className="text-xs text-gray-500 leading-none">2 min ago</span>
+                            </div>
+                            <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+                        </div>
+                        <div className="flex w-full mt-2 space-x-3 max-w-xs">
+                            <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+                            <div>
+                                <div className="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+                                    <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                                </div>
+                                <span className="text-xs text-gray-500 leading-none">2 min ago</span>
+                            </div>
+                        </div>
+
+                        <div className="flex w-full mt-2 space-x-3 max-w-xs ml-auto justify-end">
+                            <div>
+                                <div className="bg-blue-600 text-white p-3 rounded-l-lg rounded-br-lg">
+                                    <p className="text-sm">Lorem ipsum dolor sit amet.</p>
+                                </div>
+                                <span className="text-xs text-gray-500 leading-none">2 min ago</span>
+                            </div>
+                            <div className="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+                        </div>
+
+                    </div>
+
+                    <div className="bg-gray-300 p-4 flex items-center gap-3">
+                        <input className="flex items-center h-10 w-full rounded px-3 text-sm" type="text" placeholder="Type your message…" onChange={(e) => setMessage(e.target.value)} />
+
+                        <button
+                            onClick={sendMessage}
+
+                            className="flex items-center bg-blue-500 text-white gap-1 px-4 py-2 cursor-pointer text-gray-800 font-semibold tracking-widest rounded-md hover:bg-blue-400 duration-300 hover:gap-2 hover:translate-x-3"
+                        >
+                            Send
+                            <svg
+                                className="w-5 h-5"
+                                stroke="currentColor"
+                                strokeWidth="1.5"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5"
+                                    strokeLinejoin="round"
+                                    strokeLinecap="round"
+                                ></path>
+                            </svg>
+                        </button>
+
+                    </div>
+                </div>
+                {/* <!-- Component End  --> */}
+
+            </div>
+            {/* <div className="bg-white border border-slate-200 grid grid-cols-6 gap-2 rounded-xl p-2 text-sm">
                 <h1 className="text-center text-slate-800 text-xl font-bold col-span-6">Send Message</h1>
                 <input
                     onChange={(e) => setMessage(e.target.value)}
@@ -52,7 +128,7 @@ const PrivateChat = ({ socket, username, room }) => {
                     </svg>
                 </button>
 
-            </div>
+            </div> */}
             {/* <div className="chat-header">
                 <p>Live chat</p>
             </div>
